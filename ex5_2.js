786d5cbb59c4aa7ec41f69db90e0f227e3aeec2a
revital-m <revital13m@gmail.com>
2021-11-13 12:25:03 +0200

    ex5.2 - done

---
+++ ex5_2.js
@@ -0,0 +1,8 @@
+function repeatStr(count, str){
+    return str.repeat(count);
+}
+
+let resRepeatStr1 = repeatStr(6, "I");
+let resRepeatStr2 = repeatStr(5, "Hello");
+
+console.log(`ex5.2 : resRepeatStr1 = ${resRepeatStr1}, resRepeatStr2 = ${resRepeatStr2}`);
