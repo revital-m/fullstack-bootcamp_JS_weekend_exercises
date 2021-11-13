3e96212019d353b530880f19b584f5b04310a023
revital-m <revital13m@gmail.com>
2021-11-13 15:13:44 +0200

    ex5.3 - done

---
+++ ex5_3.js
@@ -0,0 +1,18 @@
+function toCamelCase(str){
+    let camelCaseStr = '';
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === '-' || str[i] === '_') {
+            camelCaseStr += str[i+1].toUpperCase();
+            i++;
+        }
+        else {
+            camelCaseStr += str[i];
+        }
+    }
+    return camelCaseStr;
+}
+
+let resCamelCase1 = toCamelCase("the-stealth-warrior");
+let resCamelCase2 = toCamelCase("The_Stealth_Warrior");
+
+console.log(`ex5.2 : resCamelCase1 = ${resCamelCase1}, resCamelCase2 = ${resCamelCase2}`);
