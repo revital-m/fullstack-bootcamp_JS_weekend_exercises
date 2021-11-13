927f0cdf5de87db73568d08dd14a680c12cd92e1
revital-m <revital13m@gmail.com>
2021-11-13 17:15:15 +0200

    ex5.6 - done

--- ex5_6.js
+++ ex5_6.js
@@ -1,7 +1,4 @@
-function maskify(str){
-    let maskStr = (str.length < 4) ? str : '#'.repeat(str.length-4) + str.slice(-4);
-    return maskStr;
-}
+let maskify = (str) => ((str.length < 4) ? str : '#'.repeat(str.length-4) + str.slice(-4));
 
 let resMaskify1 = maskify("4556364607935616");
 let resMaskify2 = maskify("64607935616");
@@ -9,6 +6,5 @@ let resMaskify3 = maskify("1");
 let resMaskify4 = maskify("");
 let resMaskify5 = maskify("Skippy");
 let resMaskify6 = maskify("Nananananananananananananananana Batman!");
-
 
 console.log(`ex5.6 : resMaskify1 = ${resMaskify1} , resMaskify2 = ${resMaskify2} , resMaskify3 = ${resMaskify3} , resMaskify4 = ${resMaskify4} , resMaskify5 = ${resMaskify5} , resMaskify6 = ${resMaskify6}`);
