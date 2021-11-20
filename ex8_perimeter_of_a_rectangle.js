e2bf956b0be6c7657ff161524f596aaf2e9efc22
revital-m <revital13m@gmail.com>
2021-11-20 19:00:38 +0200

    ex 8 - done

---
+++ ex8_perimeter_of_a_rectangle.js
@@ -0,0 +1,7 @@
+const findPerimeter = (length, width) => (length*2 + width*2);
+
+let resFindPerimeter1 = findPerimeter(6, 7);
+let resFindPerimeter2 = findPerimeter(20, 10);
+let resFindPerimeter3 = findPerimeter(2, 9);
+
+console.log(`ex8 : resFindPerimeter1 = ${resFindPerimeter1} , resFindPerimeter2 = ${resFindPerimeter2} , resFindPerimeter3 = ${resFindPerimeter3}`);
